d674882283434ab593316b3a6b6cc868cc55299f
Simon Sundell <simon.sundell@lm.se>
2022-12-05 12:15:05 +0100

    day5

--- Day5/Day5.cs
+++ Day5/Day5.cs
@@ -10,22 +10,19 @@ namespace aoc2022.Day5
 {
     public class Day5
     {
+        public static Regex MoveFromTo = new Regex("move (\\d+) from (\\d+) to (\\d+)", RegexOptions.IgnoreCase);
+
         public static void Run(string[] args)
         {
             Console.WriteLine("--- Day 5: Supply Stacks ---\r\n");
             List<List<Stack>> myList = new();
-            string[] example_lines = File.ReadAllLines("C:\\Users\\simsun\\source\\repos\\aoc2022\\Day5\\Example.txt");
-            //string[] data_file = File.ReadAllLines(".\\Day5\\Data.txt");
+            //string[] example_lines = File.ReadAllLines("C:\\Users\\simsun\\source\\repos\\aoc2022\\Day5\\Example.txt");
+            string[] data_file = File.ReadAllLines("C:\\Users\\simsun\\source\\repos\\aoc2022\\Day5\\Data.txt");
 
-            (Stack<char>[] crates, int instructionLineBeginning) = ParseInput(example_lines);
-            foreach (var crate in crates)
-            {
-                foreach(var c in crate)
-                    Console.WriteLine(c);
+         
+            Console.WriteLine(SolvePart1(data_file));
+            Console.WriteLine(SolvePart2(data_file));
 
-            }
-
-            Console.WriteLine(instructionLineBeginning);
         }
 
         static (Stack<char>[], int) ParseInput(string[] lines)
@@ -49,6 +46,44 @@ namespace aoc2022.Day5
             return (crates, initialCrates + 2);
         }
 
-        
+        public static string SolvePart1(string[] lines)
+        {
+
+            (Stack<char>[] crates, int instructionLine) = ParseInput(lines);
+            for (int i = instructionLine; i < lines.Length; i++)
+            {
+                int[] nums = MoveFromTo.Matches(lines[i])[0].Groups.Cast<Group>()
+                    .Skip(1)
+                    .Select(g => int.Parse(g.Value))
+                    .ToArray();
+
+                Enumerable.Range(0, nums[0])
+                    .Select(n => crates[nums[1] - 1]
+                    .Pop())
+                    .ToList()
+                    .ForEach(c => crates[nums[2] - 1].Push(c));
+            }
+            return new string(crates.Select(s => s.Peek()).ToArray());
+        }
+        public static string SolvePart2(string[] lines)
+        {
+            (Stack<char>[] crates, int instructionLine) = ParseInput(lines);
+            for (int i = instructionLine; i < lines.Length; i++)
+            {
+                int[] nums = MoveFromTo.Matches(lines[i])[0].Groups.Cast<Group>()
+                    .Skip(1)
+                    .Select(g => int.Parse(g.Value))
+                    .ToArray();
+
+                Enumerable.Range(0, nums[0])
+                    .Select(n => crates[nums[1] - 1]
+                    .Pop())
+                    .Reverse()
+                    .ToList()
+                    .ForEach(c => crates[nums[2] - 1].Push(c));
+            }
+            return new string(crates.Select(s => s.Peek()).ToArray());
+        }
     }
+   
 }
